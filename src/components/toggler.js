b9e7bc88472d6fee7117d9b946828f806a0a66ba
GouravMukherjee <gouravmukherjee09@gmail.com>
2019-02-04 13:00:02 +0530

    added day/night mode featureon toggler

--- src/components/toggler.js
+++ src/components/toggler.js
@@ -3,7 +3,11 @@ import React from 'react';
 class Toggler extends React.Component {
    render() {
       return(
-         <div>{this.props.toggler}</div>
+         <div 
+            className = {this.props.class} 
+            onClick = {this.props.clicked}>
+            {this.props.value !== "day" ? <i className="far fa-moon"></i>: <i class="fas fa-sun"></i> }
+         </div>
       )
    }
 }
